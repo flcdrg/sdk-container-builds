c2aae12995328e2cbd1e252751f87a2ebc44cf9a
Chet Husk <chusk3@gmail.com>
2022-09-02 14:32:20 -0500

    pull multiple images down as part of test startup

--- Test.Microsoft.NET.Build.Containers.Filesystem/DockerRegistryManager.cs
+++ Test.Microsoft.NET.Build.Containers.Filesystem/DockerRegistryManager.cs
@@ -6,13 +6,33 @@ namespace Test.Microsoft.NET.Build.Containers.Filesystem;
 public class DockerRegistryManager
 {
     public const string BaseImage = "dotnet/runtime";
+    public const string ChiseledImage = "dotnet/nightly/aspnet";
     public const string BaseImageSource = "mcr.microsoft.com/";
+    public const string ChiseledImageSource = "mcr.microsoft.com/";
     public const string BaseImageTag = "6.0";
+    public const string ChiseledImageTag = "6.0-jammy-chiseled";
     public const string LocalRegistry = "localhost:5010";
 
     public const string FullyQualifiedBaseImageDefault = $"https://{BaseImageSource}{BaseImage}:{BaseImageTag}";
 
     private static string s_registryContainerId;
+
+    public static void Ingest(string source, string dest, string image, string tag) {
+        Process pullBase = Process.Start("docker", $"pull {source}{image}:{tag}");
+        Assert.IsNotNull(pullBase);
+        pullBase.WaitForExit();
+        Assert.AreEqual(0, pullBase.ExitCode);
+
+        Process tagger = Process.Start("docker", $"tag {source}{image}:{tag} {dest}/{image}:{tag}");
+        Assert.IsNotNull(tagger);
+        tagger.WaitForExit();
+        Assert.AreEqual(0, tagger.ExitCode);
+
+        Process pushBase = Process.Start("docker", $"push {dest}/{image}:{tag}");
+        Assert.IsNotNull(pushBase);
+        pushBase.WaitForExit();
+        Assert.AreEqual(0, pushBase.ExitCode);
+    }
 
     [AssemblyInitialize]
     public static void StartAndPopulateDockerRegistry(TestContext context)
@@ -37,20 +57,8 @@ public class DockerRegistryManager
 
         s_registryContainerId = registryContainerId;
 
-        Process pullBase = Process.Start("docker", $"pull {BaseImageSource}{BaseImage}:{BaseImageTag}");
-        Assert.IsNotNull(pullBase);
-        pullBase.WaitForExit();
-        Assert.AreEqual(0, pullBase.ExitCode);
-
-        Process tag = Process.Start("docker", $"tag {BaseImageSource}{BaseImage}:{BaseImageTag} {LocalRegistry}/{BaseImage}:{BaseImageTag}");
-        Assert.IsNotNull(tag);
-        tag.WaitForExit();
-        Assert.AreEqual(0, tag.ExitCode);
-
-        Process pushBase = Process.Start("docker", $"push {LocalRegistry}/{BaseImage}:{BaseImageTag}");
-        Assert.IsNotNull(pushBase);
-        pushBase.WaitForExit();
-        Assert.AreEqual(0, pushBase.ExitCode);
+        Ingest(BaseImageSource, LocalRegistry, BaseImage, BaseImageTag);
+        Ingest(ChiseledImageSource, LocalRegistry, ChiseledImage, ChiseledImageTag);
     }
 
     [AssemblyCleanup]
